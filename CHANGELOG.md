e21ed6a18bf7469a6328736afebd82a0891667a6
Xiang-cd <xcd24@mails.tsinghua.edu.cn>
2025-05-28 20:38:05 +0800

    new build yml ready

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@
 ## v0.0.1
 
 - Initial release, with auto build and release.
+
+## v0.0.5
+
+- rewrite with more structued file.
--- thu_downloader/macos/Flutter/GeneratedPluginRegistrant.swift
+++ thu_downloader/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
