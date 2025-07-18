25cf1cb4578cca93ca9ccb35c884b2bd3e0b2499
Xiang-cd <xcd24@mails.tsinghua.edu.cn>
2025-07-18 15:54:57 +0800

    depends of webview

--- thu_downloader/macos/Flutter/GeneratedPluginRegistrant.swift
+++ thu_downloader/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import file_picker
 import shared_preferences_foundation
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
